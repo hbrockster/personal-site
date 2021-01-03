344e74f2411538630baac04e6f8604e718fe104b
hbrockster13 <50593556+hbrockster13@users.noreply.github.com>
2021-01-03 16:31:46 -0600

    Adding a section comp

---
+++ src/components/Section.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="section">
+    <div class="section__title">
+      <span class="section__number">{{ formatNumber() }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  props: {
+    number: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    methods: {
+      formatNumber() {
+        const fromattedNum = this.number + ".";
+        return this.number < 10 ? "0" + fromattedNum : fromattedNum;
+      }
+    }
+  }
+};
+</script>
+
+<style></style>
