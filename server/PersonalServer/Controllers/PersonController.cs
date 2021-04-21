01dd395b351fce401588c95cbdff7935200840b4
Hunter Brock <hbrockster13@gmail.com>
2021-04-20 22:12:43 -0500

    Added a c# backend

---
+++ server/PersonalServer/Controllers/PersonController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PersonalServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return new JsonResult(
+                new Person()
+                {
+                    Name = new Name() { First = "Hunter", Middle = "Craig", Last = "Brock" },
+                    CellPhoneNumber = "8506123487",
+                    EmailAddress = "hbrockster13@gmail.com",
+                    Id = new Guid()
+                }
+            );
+        }
+        [HttpGet("name")]
+        public JsonResult GetName()
+        {
+            return new JsonResult( new Name() { First = "Hunter", Middle = "Craig", Last = "Brock" });
+        }
+    }
+}
