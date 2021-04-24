de94422e85bbe1f5f80e652ffee585f8ff788947
Hunter Brock <hbrockster13@gmail.com>
2021-04-24 13:12:24 -0500

    Added personal site style back

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <NavigationBar />
+    <Introduction />
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import NavigationBar from '@/components/NavigationBar.vue';
+import Introduction from '@/components/Introduction.vue';
+
+@Component({ components: { NavigationBar, Introduction }})
+export default class App extends Vue {
+}
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Space Mono, Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin-top: 60px;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+* {
+    background: #272727;
+    color: #67ff88;
+    font-size: 16px;
 }
 </style>
---
+++ client/src/components/Introduction.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="intro" class="intro">
+    <div class="intro__msg">
+      Hello, my name is
+    </div>
+    <div class="intro__name">Hunter Brock</div>
+    <p>
+      I am a full stack developer/software engineer based in Florida with a
+      large set of <a href="#skills">skills</a> in front and back end web
+      development.
+    </p>
+    <!-- <button class="button">Contact</button> -->
+    <!-- <a href="#contact" class="button">Contact</a> -->
+    <button class="button" @click="getPerson()">Contact</button>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class Introduction extends Vue {
+}
+</script>
+<style>
+.intro {
+  text-align: left;
+  height: 60rem;
+}
+.intro__msg {
+  color: #cbffe5;
+}
+.intro__name {
+  font-size: 4rem;
+  font-weight: 600;
+}
+</style>
